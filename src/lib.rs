c4e76df89e2a94423f367a7bf1dba02d9b8f2de6
Zibi Braniecki <gandalf@mozilla.com>
2016-12-30 23:17:21 -0800

    Inital commit

---
+++ src/lib.rs
@@ -0,0 +1,117 @@
+#![feature(proc_macro)]
+
+extern crate serde;
+extern crate serde_json;
+
+#[macro_use]
+extern crate serde_derive;
+extern crate fluent;
+
+use fluent::syntax::runtime::ast;
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Resource(pub Vec<Entry>);
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub enum Entry {
+    Message(Message),
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Identifier(pub String);
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Message {
+    pub id: String,
+    pub value: Option<Pattern>,
+    pub traits: Option<Vec<Member>>,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Pattern {
+    pub elements: Vec<PatternElement>,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub enum PatternElement {
+    Text(String),
+    Placeable(Vec<Expression>),
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Member {
+    pub key: String,
+    pub value: Pattern,
+    pub default: bool,
+}
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub enum Expression {
+    EntityReference(Identifier),
+}
+
+impl From<ast::Resource> for Resource {
+    fn from(r: ast::Resource) -> Resource {
+        Resource(r.0
+            .into_iter()
+            .map(|e| Entry::from(e))
+            .collect::<Vec<_>>())
+    }
+}
+
+impl From<ast::Entry> for Entry {
+    fn from(e: ast::Entry) -> Entry {
+        match e {
+            ast::Entry::Message(m) => {
+                Entry::Message(Message {
+                    id: m.id,
+                    value: Some(Pattern::from(m.value.unwrap())),
+                    traits: None,
+                })
+            }
+        }
+    }
+}
+
+impl From<ast::Identifier> for Identifier {
+    fn from(i: ast::Identifier) -> Identifier {
+        Identifier(String::from("key2"))
+    }
+}
+
+impl From<ast::Pattern> for Pattern {
+    fn from(p: ast::Pattern) -> Pattern {
+        Pattern {
+            elements: p.elements
+                .into_iter()
+                .map(|e| PatternElement::from(e))
+                .collect::<Vec<_>>(),
+        }
+    }
+}
+
+impl From<ast::PatternElement> for PatternElement {
+    fn from(e: ast::PatternElement) -> PatternElement {
+        match e {
+            ast::PatternElement::Text(t) => PatternElement::Text(t),
+            ast::PatternElement::Placeable(p) => {
+                PatternElement::Placeable(p.into_iter()
+                    .map(|e| Expression::from(e))
+                    .collect::<Vec<_>>())
+            }
+        }
+    }
+}
+
+impl From<ast::Expression> for Expression {
+    fn from(e: ast::Expression) -> Expression {
+        match e {
+            ast::Expression::EntityReference(er) => {
+                Expression::EntityReference(Identifier::from(er))
+            }
+        }
+    }
+}
+
+pub fn serialize_json(res: &Resource) -> String {
+    serde_json::to_string_pretty(res).unwrap()
+}
---
+++ src/main.rs
@@ -0,0 +1,80 @@
+extern crate serde;
+extern crate serde_json;
+extern crate fluent;
+extern crate fluent_json;
+extern crate getopts;
+
+use std::fs::File;
+use std::io::Read;
+use std::io;
+use std::env;
+
+use getopts::Options;
+
+use fluent::syntax::runtime::parser::parse;
+
+use fluent_json::*;
+use fluent_json::serialize_json;
+
+fn read_file(path: &str) -> Result<String, io::Error> {
+    let mut f = try!(File::open(path));
+    let mut s = String::new();
+    try!(f.read_to_string(&mut s));
+    Ok(s)
+}
+
+fn print_resource(res: &Resource) {
+    println!("{:?}", res);
+}
+
+fn print_json_resource(res: &Resource) {
+    println!("{}", serialize_json(&res));
+}
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} FILE [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optflag("s", "silence", "disable output");
+    opts.optflag("r", "raw", "print raw result");
+    opts.optflag("h", "help", "print this help menu");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => panic!(f.to_string()),
+    };
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+    let input = if !matches.free.is_empty() {
+        matches.free[0].clone()
+    } else {
+        print_usage(&program, opts);
+        return;
+    };
+
+    let source = read_file(&input).expect("Read file failed");
+
+    let res = parse(&source);
+
+    if matches.opt_present("s") {
+        return;
+    };
+
+    match res {
+        Ok(res) => {
+            if matches.opt_present("r") {
+                print_resource(&Resource::from(res));
+            } else {
+                print_json_resource(&Resource::from(res));
+            }
+        }
+        Err(err) => println!("Error: {:?}", err),
+    };
+}
